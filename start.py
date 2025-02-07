eda09ba5b001ceecc9b4a445bfcdbf5248399602
JB-DENIS <77498359+JB-DENIS@users.noreply.github.com>
2025-02-07 15:55:24 +0100

    Create start.py

---
+++ start.py
@@ -0,0 +1,44 @@
+import multiprocessing
+import subprocess
+import os
+
+
+def start_backend():
+    os.chdir("/home/user/app/backend")
+    subprocess.run(["uv", "sync"])
+    subprocess.run(
+        ["uv", "run", "fastapi", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+    )
+
+
+def start_frontend():
+    os.chdir("/home/user/app/frontend")
+    subprocess.run(["uv", "sync"])
+    subprocess.run(
+        [
+            "uv",
+            "run",
+            "streamlit",
+            "run",
+            "app/main.py",
+            "--server.port",
+            "8501",
+            "--server.address",
+            "0.0.0.0",
+        ]
+    )
+
+
+def main():
+    backend_process = multiprocessing.Process(target=start_backend)
+    frontend_process = multiprocessing.Process(target=start_frontend)
+
+    backend_process.start()
+    frontend_process.start()
+
+    backend_process.join()
+    frontend_process.join()
+
+
+if __name__ == "__main__":
+    main()
